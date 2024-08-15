4c9b870ddd20ce9a3be2631b78077772cf38229f
Cristian <cristian.guamba>
2024-08-14 20:58:00 -0500

    Testing

---
+++ TestProject1/Service/NoticiaServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using LabReciclaje.Service;
+using FluentAssertions;
+
+namespace TestProject1.Service
+{
+    public class NoticiaServiceTests
+    {
+        NoticiaService noticiaService;
+        [Fact]
+        public async void GetNoticiaAsyncTest()
+        {            
+            //Arrange
+            noticiaService = new NoticiaService();
+
+            //Act
+            var noticia = await noticiaService.GetNoticiaAsync();
+
+            //Assert
+            noticia.Should().NotBeNull();
+        }
+    }
+}
